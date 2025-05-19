a93e5b6b541113a9d85b7a7b2162dcd6e4e6cb72
Sufyan <sufyanmalik@hotmail.com>
2025-05-19 10:17:10 +0100

    working product desc generator

--- app/page.js
+++ app/page.js
@@ -31,6 +31,12 @@ export default function Home() {
         >
           Realtime Chat
         </Link>
+        <Link
+          href="/product-description-generator"
+          className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-md transition duration-200 block text-center"
+        >
+          Multimodal Product Description Generator
+        </Link>
       </div>
     </div>
   );
